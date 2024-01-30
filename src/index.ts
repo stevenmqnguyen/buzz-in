116c232155b2c4c158b358c05ef1b9db2baf4633
Steven Nguyen <34099426+Stevenmqnguyen@users.noreply.github.com>
2024-01-30 01:54:39 -0600

    add buzzing in response

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,17 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import VoiceResponse from 'twilio/lib/twiml/VoiceResponse';
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
+export interface Env {}
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		let twiml = new VoiceResponse();
+
+		twiml.say('Buzzing in');
+
+		twiml.play({
+			digits: 'ww9w9',
+		});
+
+		return new Response(twiml.toString(), { headers: { 'Content-Type': 'application/xml' } });
 	},
 };
