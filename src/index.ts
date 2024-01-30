de6cb90729865c287bb761787dc7f5f7f0d117a0
Steven Nguyen <34099426+Stevenmqnguyen@users.noreply.github.com>
2024-01-30 02:58:09 -0600

    feat: pushover notifications

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
 import VoiceResponse from 'twilio/lib/twiml/VoiceResponse';
 
-export interface Env {}
+export interface Env {
+	PUSHOVER_APP_TOKEN: string;
+	PUSHOVER_USER_TOKEN: string;
+}
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
@@ -11,6 +24,25 @@ export default {
 		twiml.play({
 			digits: 'ww9w9',
 		});
+
+		const pushover_params = {
+			token: env.PUSHOVER_APP_TOKEN,
+			user: env.PUSHOVER_USER_TOKEN,
+			title: 'Buzz-In',
+			message: 'Somebody buzzed in at the gate!',
+		};
+
+		try {
+			const response = await fetch('https://api.pushover.net/1/messages.json', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(pushover_params),
+			});
+		} catch (error) {
+			console.log('Error:', error);
+		}
 
 		return new Response(twiml.toString(), { headers: { 'Content-Type': 'application/xml' } });
 	},
